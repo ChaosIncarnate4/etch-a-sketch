32670fee84838204b05e8e377e2898e05afa15b9
Wilson <wj0503@gmail.com>
2025-11-26 09:18:35 -0500

    Added size-altering function

--- script.js
+++ script.js
@@ -1,17 +1,60 @@
-function createGrid() {
+function createGrid(size = 16) {
     const container = document.querySelector("#container");
-    for (let i = 0; i < 16; i++) {
+    container.innerHTML = ""
+
+    for (let i = 0; i < size; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
 
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < size; j++) {
             const box = document.createElement("div");
             box.classList.add("gridTile");
             row.appendChild(box);
         }
-        
+
         container.appendChild(row);
     }
+
+    draw();
 }
 
 createGrid();
+
+
+
+function getGridSize() {
+    while (true) {
+        const size = prompt("Input grid size.");
+
+        if (size >= 1 && size <= 100) {
+            return size;
+        }
+
+        alert("Size must be at least 1 and at most 100.");
+    }    
+}
+
+
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    let size = getGridSize();
+    createGrid(size);
+})
+
+
+
+function draw() {
+    const grid = document.querySelectorAll(".gridTile");
+    grid.forEach(tile => {
+    tile.addEventListener("mouseover", () => {
+        tile.style.backgroundColor = "black";
+    });
+});
+}
+
+draw();
+
+
+
+
